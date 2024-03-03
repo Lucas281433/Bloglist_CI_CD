0df13aff04cee6c55cced0db0d7324d9acd72f9a
Lucas281433 <lucas2814dev@gmail.com>
2024-03-03 11:55:34 -0300

    Exercise 11.21

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-const middleware = require('./utils/middleware')
+const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const healthRouter = require("./controllers/health");
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -21,18 +22,19 @@ mongoose
   });
 
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 app.use(cors());
-app.use(middleware.tokenExtractor)
+app.use(middleware.tokenExtractor);
 
+app.use("/api/health", healthRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
-if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
 }
 
-app.use(middleware.errorHandler)
+app.use(middleware.errorHandler);
 
 module.exports = app;
